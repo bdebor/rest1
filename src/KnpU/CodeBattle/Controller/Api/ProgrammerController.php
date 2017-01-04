4d0bdf6717914465e9b80563494249b2449469f3
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-01-04 07:17:14 +0100

    7. GET’ING RESOURCES, AND CONTENT-TYPE

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -3,6 +3,7 @@
 namespace KnpU\CodeBattle\Controller\Api;
 
 use KnpU\CodeBattle\Controller\BaseController;
+use KnpU\CodeBattle\Model\Programmer;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,57 @@ class ProgrammerController extends BaseController
 {
     protected function addRoutes(ControllerCollection $controllers)
     {
-        // $controllers->post('/api/programmers', array($this, 'newAction'));
+        $controllers->post('/api/programmers', array($this, 'newAction'));
+        $controllers->get('/api/programmers/{nickname}', array($this, 'showAction'));
+    }
+
+    public function newAction(Request $request)
+    {
+        // return 'let\'s battle!'; // 5
+        /**/ // 6
+//        $data = $request->getContent();
+//        return $data;
+
+        $data = json_decode($request->getContent(), true);
+
+        $programmer = new Programmer($data['nickname'], $data['avatarNumber']);
+        $programmer->tagLine = $data['tagLine'];
+        $programmer->userId = $this->findUserByUsername('weaverryan')->id;
+
+        $this->save($programmer);
+
+//        return 'It worked. Believe me - I\'m an API';
+
+//        return new Response('It worked. Believe me - I\'m an API', 201);
+
+        $response = new Response('It worked. Believe me - I\'m an API', 201);
+        $response->headers->set('Location', '/some/programmer/url');
+
+        return $response;
+
+        /*/*/
+    }
+
+    public function showAction($nickname)
+    {
+//        return 'Hello '.$nickname;
+        $programmer = $this->getProgrammerRepository()->findOneByNickname($nickname);
+
+        if (!$programmer) {
+            $this->throw404('Crap! This programmer has deserted! We\'ll send a search party');
+        }
+
+        $data = array(
+            'nickname' => $programmer->nickname,
+            'avatarNumber' => $programmer->avatarNumber,
+            'powerLevel' => $programmer->powerLevel,
+            'tagLine' => $programmer->tagLine,
+        );
+
+//        return new Response(json_encode($data), 200);
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
 }
