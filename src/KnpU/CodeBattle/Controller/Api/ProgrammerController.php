86d85bd042833a3bb76e9531c125fa1b731ae0b1
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-01-04 12:50:38 +0100

    10. FIXING THE CONTENT-TYPE ON POST

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -15,7 +15,8 @@ class ProgrammerController extends BaseController
     protected function addRoutes(ControllerCollection $controllers)
     {
         $controllers->post('/api/programmers', array($this, 'newAction'));
-        $controllers->get('/api/programmers/{nickname}', array($this, 'showAction'))->bind('api_programmers_show');
+$controllers->get('/api/programmers/{nickname}', array($this, 'showAction'))->bind('api_programmers_show');
+        $controllers->get('/api/programmers', array($this, 'listAction'));
     }
 
     public function newAction(Request $request)
@@ -46,16 +47,35 @@ class ProgrammerController extends BaseController
             $this->throw404('Crap! This programmer has deserted! We\'ll send a search party');
         }
 
-        $data = array(
+        $data = $this->serializeProgrammer($programmer);
+
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    public function listAction()
+    {
+        $programmers = $this->getProgrammerRepository()->findall();
+        $data = array('programmers' => array());
+        foreach($programmers as $programmer){
+            $data['programmers'][] = $this->serializeProgrammer($programmer);
+        }
+
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    private function serializeProgrammer(Programmer $programmer)
+    {
+        return array(
             'nickname' => $programmer->nickname,
             'avatarNumber' => $programmer->avatarNumber,
             'powerLevel' => $programmer->powerLevel,
             'tagLine' => $programmer->tagLine,
         );
-
-        $response = new Response(json_encode($data), 200);
-        $response->headers->set('Content-Type', 'application/json');
-        return $response;
     }
-
 }
