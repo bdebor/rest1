c2945775ea317ec7b40b405ab1062feac0922229
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-01-05 13:00:10 +0100

    26. HANDLING 404 ERRORS

--- features/api/programmer.feature
+++ features/api/programmer.feature
@@ -117,3 +117,10 @@ Feature: Programmer
     Then the response status code should be 400
     And the "Content-Type" header should be "application/problem+json"
     And the "type" property should equal "invalid_body_format"
+
+  Scenario: Proper 404 exception on no programmer
+    When I request "GET /api/programmers/fake"
+    Then the response status code should be 404
+    And the "Content-Type" header should be "application/problem+json"
+    And the "type" property should equal "about:blank"
+    And the "title" property should equal "Not Found"
--- src/KnpU/CodeBattle/Api/ApiProblem.php
+++ src/KnpU/CodeBattle/Api/ApiProblem.php
@@ -25,16 +25,24 @@ class ApiProblem
 
     private $extraData = array();
 
-    public function __construct($statusCode, $type)
+    public function __construct($statusCode, $type = null)
     {
         $this->statusCode = $statusCode;
         $this->type = $type;
+		if (!$type) {
+			// no type? The default is about:blank and the title should
+			// be the standard status code message
+			$this->type = 'about:blank';
+			$this->title = isset(Response::$statusTexts[$statusCode])
+				? Response::$statusTexts[$statusCode]
+				: 'Unknown HTTP status code :(';
+		} else {
+			if (!isset(self::$titles[$type])) {
+				throw new \InvalidArgumentException('No title for type '.$type);
+			}
 
-        if (!isset(self::$titles[$type])) {
-            throw new \InvalidArgumentException('No title for type '.$type);
-        }
-
-        $this->title = self::$titles[$type];
+			$this->title = self::$titles[$type];
+		}
     }
 
     public function getStatusCode()
