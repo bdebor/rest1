6fe7109b31c1bc6b28c18006cb5f94e42d3b9f2b
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-01-06 09:18:26 +0100

    3. SERIALIZER ANNOTATIONS

--- src/KnpU/CodeBattle/Model/Programmer.php
+++ src/KnpU/CodeBattle/Model/Programmer.php
@@ -4,29 +4,40 @@ namespace KnpU\CodeBattle\Model;
 
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+use JMS\Serializer\Annotation as Serializer;
 
+/**
+ * @Serializer\ExclusionPolicy("all")
+ */
 class Programmer
 {
-
     /* All public properties are persisted */
     public $id;
 
     /**
      * @Assert\NotBlank(message="Please enter a clever nickname")
+     * @Serializer\Expose
      */
     public $nickname;
 
     /**
      * Number of an avatar, from 1-6
+     * @Serializer\Expose
      *
      * @var integer
      */
     public $avatarNumber;
 
+    /**
+     * @Serializer\Expose
+     */
     public $tagLine;
 
     public $userId;
 
+    /**
+     * @Serializer\Expose
+     */
     public $powerLevel = 0;
 
     public function __construct($nickname = null, $avatarNumber = null)
@@ -34,6 +45,4 @@ class Programmer
         $this->nickname = $nickname;
         $this->avatarNumber = $avatarNumber;
     }
-
-
 }
