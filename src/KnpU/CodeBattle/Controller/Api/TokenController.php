ac1796943b5041c9a01fc44ec7450b8b3986422b
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-01-06 16:55:19 +0100

    Creating the Token Resource

--- src/KnpU/CodeBattle/Controller/Api/TokenController.php
+++ src/KnpU/CodeBattle/Controller/Api/TokenController.php
@@ -3,7 +3,9 @@
 namespace KnpU\CodeBattle\Controller\Api;
 
 use KnpU\CodeBattle\Controller\BaseController;
+use KnpU\CodeBattle\Security\Token\ApiToken;
 use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Request;
 
 class TokenController extends BaseController
 {
@@ -12,8 +14,18 @@ class TokenController extends BaseController
         $controllers->post('/api/tokens', array($this, 'newAction'));
     }
 
-    public function newAction()
+    public function newAction(Request $request)
     {
-        return 'foo';
+        $username = $request->headers->get('PHP_AUTH_USER');
+        $user = $this->getUserRepository()->findUserByUsername($username);
+
+        $data = json_decode($request->getContent(), true);
+
+        $token = new ApiToken($user->id);
+        $token->notes = $data['notes'];
+
+        $this->getApiTokenRepository()->save($token);
+
+        return $this->createApiResponse($token, 201);
     }
 }
